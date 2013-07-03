18ad6bc0c97bad8e55e1dc42c40bb73a3582f9ab
Apprentice <apprentice@devbootcamp.com>
2013-07-03 15:11:48 -0700

    Started new Rails project to squash Craigslist forever

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roominatr::Application.config.secret_token = '1d1a6ce785ba11fa8b502e11b90931da237b428958af6bd77c7db80386c61013d5f393eaa39e3085b9fb6e377d7425a2d4826bb710e3d95cfdc2768831af8285'
