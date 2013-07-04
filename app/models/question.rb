4f525eeafe42758f70cc6d768a20f688b525ec42
Apprentice <apprentice@devbootcamp.com>
2013-07-04 12:17:59 -0700

    Can automatically create answers for the 2nd and 3rd questions of a survey

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,16 +9,29 @@ class Question < ActiveRecord::Base
   attr_accessible :body, :qtype, :answers_attributes
   #IF type roommate, body = "How would you like your roommmate to answer"
 
-  after_create :set_body
+  after_create :set_body, :create_answers
 
     #If type roommate, body = "how woudl you like rmm to answer"
   private
 
   def set_body
     if qtype == "roommate"
-      body = "How would you like your roommate to answer?"
-    else
-      body = "How important is this to you?"
+      update_attribute("body", "How would you like your roommate to answer?")
+    elsif qtype == "importance"
+      update_attribute("body","How important is this to you?")
+    end
+  end
+
+  def create_answers
+    if qtype == "roommate"
+      me_question = survey.questions.first
+      me_question.answers.each do |answer|
+        answers.create(text: answer.text,
+                       weight: answer.weight)
+      end
+    elsif qtype == "importance"
+      options = [["Not", 1], ["Kinda", 2], ["Very", 3]]
+      options.each { |o| answers.create(text: o[0], weight: o[1])}
     end
   end
 
