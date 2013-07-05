d8592bdd4e73c9ae4fececbdbb0eaa0b305116e2
Apprentice <apprentice@devbootcamp.com>
2013-07-04 18:47:14 -0700

    Created answer spec

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Answer do
+
+  describe "#initialize" do
+
+    it { should belong_to(:question) }
+    it { should allow_mass_assignment_of(:weight) }
+    it { should allow_mass_assignment_of(:text) }
+
+  end
+
+end
