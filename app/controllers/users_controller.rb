dba5d89c2705481b119a56546a29243b89284269
Apprentice <apprentice@devbootcamp.com>
2013-07-04 20:21:42 -0700

    User can't edit profiles that aren't theirs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
 	def update
 		@user = User.find(params[:id]) 
-		if @user.update_attributes(params[:user])
+		if @user.update_attributes(params[:user]) 
 			flash[:success] = "Profile Updated!"
 			redirect_to @user
 		else
