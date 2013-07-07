14c4e8032d9500a2814d77d1f99661baf4b4e469
William Bendix <williambendix@Williams-MacBook-Air.local>
2013-07-06 17:48:31 -0700

    added search in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,4 +69,8 @@ class UsersController < ApplicationController
 		redirect_to new_user_path
 	end
 
+	def search
+		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Roominatr::Application.routes.draw do
   match '/users/:id/picture', to: 'users#update_picture', via: :put, as: 'user_picture'
   match '/users/:id/password', to: 'users#edit_password', via: :get, as: 'user_password'
   match '/users/:id/password', to: 'users#update_password', via: :put, as: 'user_password'
-
+  match '/users/search', to: 'users#search', :via => :get, as: 'users_search' 
+  
   resources :sessions, :only => [:new, :create, :destroy]
   match '/signout', to: 'sessions#destroy'
 
