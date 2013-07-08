e4d4212a0b9aca785b0405a5ac3f1a8428745360
DBC-Apprentice <dbc@dbc.com>
2013-07-07 17:33:27 -0700

    wip trying to get progress bar to dynamically update

--- app/assets/javascripts/user_take_survey.js
+++ app/assets/javascripts/user_take_survey.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
   $('#new_survey_button').on('ajax:success', function(event, data, status, xhr) {
-    console.log("hey");
     console.log(data.submission_form);
     $('#new_survey').html(data.submission_form);
   });
@@ -9,6 +8,7 @@ $(document).ready(function(){
   $('#new_survey').on('ajax:success', '#create_submission', function(event, data, status, xhr) {
     if (data.new_submission) {
       $('#submission_list').append($(data.new_submission));
+      $('#blank').html('<%= escape_javascript(render :partial => "show_progress") %>');
       $('#new_survey').empty();
     } else {
       $('#new_survey').html(data.submission_form);
