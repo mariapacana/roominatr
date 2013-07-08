690e0573c276f4acc10cc60ce70183d4fe8a5323
Maria Pacana <maria.pacana@gmail.com>
2013-07-08 13:45:54 -0700

    User profile progress bar now dynamically updates! Also, consolidated all javascripts relating to survey creation/editing/etc into one file.

--- app/assets/javascripts/user_take_survey.js
+++ app/assets/javascripts/user_take_survey.js
@@ -1,17 +1,57 @@
+var Bar = function(numUserSubmissions, totalSurveys){
+  this.numUserSubmissions = numUserSubmissions;
+  this.totalSurveys = totalSurveys;
+
+  this.percentage = function() {
+    var percentage = ((this.numUserSubmissions/this.totalSurveys).toFixed(2)*100).toString()+'%';
+    return percentage;
+  }
+
+  this.upMeter = function(){
+    this.numUserSubmissions++;
+    $('.meter span').css('width', this.percentage());
+  }
+
+  this.downMeter = function(){
+    this.numUserSubmissions--;
+    $('.meter span').css('width', this.percentage());
+  }
+};
+
 $(document).ready(function(){
 
   $('#new_survey_button').on('ajax:success', function(event, data, status, xhr) {
-    console.log(data.submission_form);
     $('#new_survey').html(data.submission_form);
   });
 
   $('#new_survey').on('ajax:success', '#create_submission', function(event, data, status, xhr) {
     if (data.new_submission) {
       $('#submission_list').append($(data.new_submission));
-      $('#blank').html('<%= escape_javascript(render :partial => "show_progress") %>');
       $('#new_survey').empty();
+      bar.upMeter();
     } else {
       $('#new_survey').html(data.submission_form);
     }
   });
+
+  //This handles both editing AND deleting at once and probably ought to be refactored.
+  $('#submission_list').on('ajax:success','.submission', function(event, data, status, xhr){
+    var index = $('.submission').index($(this));
+    if (data.submission_form) {
+      $('.submission').eq(index).html(data.submission_form);
+    } else {
+      $('.submission').eq(index).remove();
+      bar.downMeter();
+    }
+  });
+
+  $('#submission_list').on('ajax:success', '.update_submission', function(event, data, status, xhr){
+    if (data.updated_submission) {
+      $(this).parent().replaceWith(data.updated_submission);
+    } else {
+      $(this).replaceWith(data.submission_form);
+    }
+  });
+
 });
+
