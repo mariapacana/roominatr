1ca7311687d72f3678d064105737b8d31882dce6
Maria Pacana <maria.pacana@gmail.com>
2013-08-31 16:34:55 -0700

    WIP now displays the partial showing the next top users

--- app/assets/javascripts/show_more_top_users.js
+++ app/assets/javascripts/show_more_top_users.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 
   $('.top_users_form').on('ajax:success', function(event, data, status, xhr){
-    console.log(data.more_users);
-    console.log("hey");
+    $("#top_users").before(data.more_users);
   });
 
 });
